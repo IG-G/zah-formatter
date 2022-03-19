86bec891ae478468dc80f4e2c789f3a76a30d8e6
Ig-G <igolos@op.pl>
2022-03-19 16:14:21 +0100

    bug fix

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ from typing import List, Tuple
 
 def prepare_list_of_tasks_assigned_to_processors(input_lines: List) -> List:
     elements = []
+    print(len(input_lines))
     for i in range(0, len(input_lines), 4):
+        print(input_lines)
         elements.append([input_lines[i], input_lines[i + 1], input_lines[i + 2], input_lines[i + 3]])
     time_q = [int(i[2][4:-1]) for i in elements]
     proc_name = [i[0][4:-1] for i in elements]
@@ -45,10 +47,10 @@ with open("input.txt") as f:
     input_lines_from_file = []
     for count, line in enumerate(f):
         input_lines_from_file.append(line)
-    assigned_list = prepare_list_of_tasks_assigned_to_processors(input_lines_from_file)
-    procA, procB, procC = split_list_to_processors(assigned_list)
-    max_element = max(len(procA), len(procB), len(procC))
-    procA = add_missing_values(procA, max_element)
-    procB = add_missing_values(procB, max_element)
-    procC = add_missing_values(procC, max_element)
-    print_table(procA, procB, procC)
+assigned_list = prepare_list_of_tasks_assigned_to_processors(input_lines_from_file)
+procA, procB, procC = split_list_to_processors(assigned_list)
+max_element = max(len(procA), len(procB), len(procC))
+procA = add_missing_values(procA, max_element)
+procB = add_missing_values(procB, max_element)
+procC = add_missing_values(procC, max_element)
+print_table(procA, procB, procC)
